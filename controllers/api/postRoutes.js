61267ee440fda50b487d68533a018b05a95fdaee
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-10 12:49:29 -0500

    added postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,4 +1,40 @@
 const router = require('express').Router()
 const { Post } = require('../../models')
 
+router.post('/', async( req,res) => {
+    try{
+        const newPost = await Post.create({
+            ...req.body,
+            userId: req.session.userId
+        })
+
+        res.status(200).json(newPost)
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    try{
+
+        const postData = await Post.destroy({
+            where:{
+                id: req.params.id,
+                userId: req.session.userId
+            }
+        })
+
+        if (!postData){
+            res.status(400).json({message: 'No post found to delete'})
+        }
+
+        res.status(200).json(postData)
+
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 module.exports= router
