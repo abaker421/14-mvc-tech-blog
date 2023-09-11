8a0219ff73b9e86a26199f5874a021d3a5a3ce42
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-11 09:37:24 -0500

    resolved connections.js errors

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,21 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
+require('dotenv').config()
 
+let sequelize;
 
-const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'blog_post_db'
-})
+if (process.env.JAWSDB_URL) {
+        sequelize = new Sequelize(process.env.JAWSDB_URL);
+      } else {
+        sequelize = new Sequelize(
+          process.env.DB_NAME,
+          process.env.DB_USER,
+          process.env.DB_PASSWORD,
+          {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+          }
+        )
+      }
 
-module.exports= connection
+module.exports = sequelize
