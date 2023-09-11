8a5a0fadb1f9567f1a52e7936ef4a7f8b421cfe2
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-11 08:32:57 -0500

    added blank values to post and user models

--- models/Post.js
+++ models/Post.js
@@ -3,4 +3,22 @@ const sequelize = require('../config/connection')
 
 class Post extends Model {}
 
+Post.init({
+
+id: {},
+title: {},
+content: {},
+authorId: {},
+dateCreated: {}
+},
+{
+    hooks: {},
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+}
+)
+
 module.exports= Post
--- models/User.js
+++ models/User.js
@@ -1,6 +1,26 @@
 const {Model, DataTypes} = require('sequelize')
 const sequelize = require('../config/connection')
+const bcrypt= require('bcrypt')
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password)
+    }
+}
+
+User.init({
+    id:{},
+    name: {},
+    email: {},
+    password:{},
+},
+{
+    hooks: {},
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+})
 
 module.exports= User
